6a017c2ae08337dc365bc7d7d62122051f507596
umakrishnamoorthy <umakrishnamoorthy27@gmail.com>
2022-01-26 15:53:12 -0800

    Adding comments

--- removeduplicatessortedarray.py
+++ removeduplicatessortedarray.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+'''remove the duplicates in an sorted array - the number should be unique in an array with no repeated numbers, these are arranged in non-decresing order'''
 def removeduplicate(self, nums):
     length = 1
     if len(nums) == 0:
